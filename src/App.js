d923d6103521b4e1550b43ddc5540fe2ebd1b79e
Abderraouf Zine <rofazayn@gmail.com>
2021-02-24 11:59:59 +0100

    Create logo component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './app.scss';
+import Logo from './components/Logo';
 import MapCanvas from './components/MapCanvas';
 import { DataProvider } from './context/DataContext';
 
 function App() {
   return (
     <div className='App'>
+      <Logo />
       <DataProvider>
         <MapCanvas />
       </DataProvider>
